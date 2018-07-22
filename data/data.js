b0de0e37255fcb1fd9da4b37343a924c1d0a8517
asq_0401 <asq3456922@126.com>
2018-07-22 15:30:53 +0800

    first commit

--- data/data.js
+++ data/data.js
@@ -4,13 +4,13 @@ var data = [
 
 
 { id: 1, name: 'phone',type:"程序开发", url: 'img/design/phone.jpg' },
-{ id: 2, name: '小程序',type:"程序开发", url: 'img/design/小程序.png' },
+{ id: 2, name: '小程序',type:"程序开发", url: 'img/design/xi.png' },
 { id: 3, name: '手机', type: "平面设计", url: 'img/design/su.png' },
-{ id: 4, name: '手机', type: "平面设计", url: 'img/design/模仿.jpg' },
+{ id: 4, name: '手机', type: "平面设计", url: 'img/design/mo.jpg' },
 { id: 5, name: 'phone', type: "平面设计", url: 'img/design/a.jpg' },
 { id: 6, name: '小程序', type: "平面设计", url: 'img/design/b.jpg' },
 { id: 7, name: '手机', type: "平面设计", url: 'img/design/nootbook.png' },
-{ id: 8, name: '手机', type: "平面设计", url: 'img/design/可莱丝面膜.jpg' }
+{ id: 8, name: '手机', type: "平面设计", url: 'img/design/ke.jpg' }
 
 
 ]
