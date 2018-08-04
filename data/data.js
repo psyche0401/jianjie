093b0147f7c1850e30448c81d993423fc864b627
psyche0401 <41417829+psyche0401@users.noreply.github.com>
2018-08-05 03:19:26 +0800

    Update data.js

--- data/data.js
+++ data/data.js
@@ -7,7 +7,7 @@ var data = [
 { id: 2, name: '小程序',type:"程序开发", url: 'img/design/xi.png' },
 { id: 3, name: '手机', type: "平面设计", url: 'img/design/su.png' },
 { id: 4, name: '手机', type: "平面设计", url: 'img/design/mo.jpg' },
-{ id: 5, name: 'phone', type: "平面设计", url: 'img/design/a.jpg' },
+{ id: 5, name: 'phone', type: "平面设计", url: 'img/design/13132.jpg' },
 { id: 6, name: '小程序', type: "平面设计", url: 'img/design/b.jpg' },
 { id: 7, name: '手机', type: "平面设计", url: 'img/design/nootbook.png' },
 { id: 8, name: '手机', type: "平面设计", url: 'img/design/ke.jpg' }
